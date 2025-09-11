43b3a9f7988aeb32c6978f284c2c7698997587a0
Mickael <mickael.mesloub@gmail.com>
2025-09-11 18:24:59 +0200

    refactor: edit styles of some components + extract Heading2 and Text from TheBasics.tsx to their own files + change wordings

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -37,7 +37,7 @@ function ToggleThemeBtn({
   const { theme } = useThemeContext()
   const isDarkTheme = theme === "dark"
   const baseStyle =
-    "text-secondary-500 outline-secondary hover:bg-secondary-500 hover:text-primary-500 focus-visible:bg-secondary-500 focus-visible:text-primary-500 rounded-full p-2 text-xl outline-2 transition-colors ml-auto"
+    "text-primary-500 outline-primary hover:bg-primary-500 hover:text-accent-600 focus-visible:bg-primary-500 focus-visible:text-accent-600 rounded-full p-2 text-xl outline-2 transition-colors ml-auto"
   return (
     <Button
       title={`Toggle ${isDarkTheme ? "light" : "dark"} mode`}
