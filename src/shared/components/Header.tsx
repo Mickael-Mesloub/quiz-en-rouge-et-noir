76f0fbb490ecc5db44723962ba5b0f514fbebfa2
Mickael <mickael.mesloub@gmail.com>
2025-09-12 15:08:44 +0200

    [WIP] refactor+style: rename QuestionList and QuestionListItem + create separate components + edit styles

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,18 +1,26 @@
-import { GoSun } from "react-icons/go";
-import { IoMoon } from "react-icons/io5";
 import { twMerge } from "tailwind-merge";
 import { useThemeContext } from "@/shared/hooks";
 import { Button } from "@/shared/components/ui";
+import { HEADER_HEIGHT } from "@/shared/constants/layout";
+import { House, Moon, Sun } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
   const { theme, changeTheme } = useThemeContext();
   const isDarkTheme = theme === "dark";
-  const handleClick = (_e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleSwitchTheme = (_e: React.MouseEvent<HTMLButtonElement>) => {
     changeTheme(isDarkTheme ? "light" : "dark");
   };
+
+  const handleNavigateToHome = () => {
+    navigate("/");
+  };
+
   return (
     <StyledHeader>
-      <ToggleThemeBtn onClick={handleClick}></ToggleThemeBtn>
+      <GoToHomeBtn onClick={handleNavigateToHome} />
+      <ToggleThemeBtn onClick={handleSwitchTheme}></ToggleThemeBtn>
     </StyledHeader>
   );
 }
@@ -23,8 +31,13 @@ function StyledHeader({
   ...restProps
 }: React.HTMLProps<HTMLHeadElement>) {
   const baseStyle = "sticky top-0 p-4 text-right";
+
   return (
-    <header className={twMerge(baseStyle, className)} {...restProps}>
+    <header
+      className={twMerge(baseStyle, className)}
+      style={{ height: HEADER_HEIGHT }}
+      {...restProps}
+    >
       {children}
     </header>
   );
@@ -45,10 +58,24 @@ function ToggleThemeBtn({
       {...restProps}
     >
       {isDarkTheme ? (
-        <GoSun aria-label="Toggle light mode" />
+        <Sun aria-label="Toggle light mode" />
       ) : (
-        <IoMoon aria-label="Toggle dark mode" />
+        <Moon aria-label="Toggle dark mode" />
       )}
+    </Button>
+  );
+}
+
+function GoToHomeBtn({
+  className = "",
+  ...restProps
+}: React.ComponentProps<typeof Button>) {
+  const baseStyle =
+    "fixed top-4 left-4 text-primary-500 outline-primary hover:bg-primary-500 hover:text-accent-600 focus-visible:bg-primary-500 focus-visible:text-accent-600 rounded-full p-2 text-xl outline-2 transition-colors ml-auto";
+
+  return (
+    <Button className={twMerge(baseStyle, className)} {...restProps}>
+      <House aria-label="Go to home screen" />
     </Button>
   );
 }
--- src/shared/components/Quizz/Quizz.tsx
+++ src/shared/components/Quizz/Quizz.tsx
@@ -1,15 +1,9 @@
-import QuestionsList from "@/shared/components/Quizz/QuestionList";
-import { Heading2 } from "@/shared/components/ui";
+import QuestionCard from "@/shared/components/Quizz/QuestionCard";
 import questionsData from "@/data/questions.json";
 import { Question } from "@/shared/types";
 
 export default function Quizz() {
   const questions: Question[] = questionsData;
 
-  return (
-    <div className="p-8">
-      <Heading2 className="text-4xl">{"Bienvenue sur le quiz"}</Heading2>
-      <QuestionsList questions={questions} />
-    </div>
-  );
+  return <QuestionCard questions={questions} />;
 }
