10747d5aec2d24b12d0d4f722d0f2ace82f0a5fb
Mickael <mickael.mesloub@gmail.com>
2025-09-12 15:42:37 +0200

    feat(quizz): implement shuffle function to generate random questions

--- src/shared/components/Quizz/Quizz.tsx
+++ src/shared/components/Quizz/Quizz.tsx
@@ -1,9 +1,12 @@
 import QuestionCard from "@/shared/components/Quizz/QuestionCard";
 import questionsData from "@/data/questions.json";
 import { Question } from "@/shared/types";
+import { shuffle } from "@/shared/utils/array.utils";
+
+const questions: Question[] = questionsData;
+
+const shuffledQuestions = shuffle(questions);
 
 export default function Quizz() {
-  const questions: Question[] = questionsData;
-
-  return <QuestionCard questions={questions} />;
+  return <QuestionCard questions={shuffledQuestions} />;
 }
